a3b5664f1a27dac37f3cda68ec8f4ed6266efa73
Eric ShangKuan <ericsk@gmail.com>
2014-04-29 23:03:29 +0800

    上傳第一章的範例

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-wp81app-samples-js
-==================
+# Windows Phone 開發實戰: 使用 JavaScript 範例
 
-Windows Phone 8.1 App 開發實戰: 使用 JavaScript/HTML5
+這裡放了所有在「Windows Phone 開發實戰: 使用 JavaScript」課程中所有的教學範例。
+
+--
+
+  - Module1
+	  - *First Windows Phone App*
+		
+		第一個 Windows Phone app，用來瞭解如何建置 Windows Phone app、如何使用 JavaScript/HTML5 來開發 Windows Phone app，以及簡單認識 WinJS 函式庫及 Windows 執行階段 (Windows Runtime) 的元件。
+
+--
+
+# 安裝開發環境
+
+  - 作業系統需要 Windows 8.1 專業版/企業版 64 位元版本。
+  - 開發工具需要 Visual Studio 2013 Update 2 以上。
+	  - Express 版請下載 Visual Studio Express 2013 for Windows 版。
---
+++ module1/First Windows Phone App/First Windows Phone App/js/default.js
@@ -0,0 +1,69 @@
+﻿(function () {
+  "use strict";
+
+  var app = WinJS.Application;
+  var activation = Windows.ApplicationModel.Activation;
+
+  // default.html 頁面 UI 的初始化
+  var init = function () {
+    // initialize the canvas
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, 400, 300);
+
+    // 註冊相機按鈕的事件 -> 叫出 FileOpenPicker 選 jpg 檔
+    var cameraBtn = document.getElementById('cmdTakePhoto');
+    cameraBtn.addEventListener('click', function (evt) {
+      // 使用 Windows Runtime 提供的 FileOpenPicker 元件
+      var fileOpenPicker = new Windows.Storage.Pickers.FileOpenPicker();
+      fileOpenPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.thumbnail;
+      fileOpenPicker.fileTypeFilter.replaceAll([".jpg"]);
+      fileOpenPicker.pickSingleFileAndContinue();
+    }, false);
+  };
+
+  app.onactivated = function (args) {
+    if (args.detail.kind === activation.ActivationKind.launch) {
+      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+
+      } else {
+
+      }
+      args.setPromise(WinJS.UI.processAll());
+
+      // 初始化
+      init();
+    }
+
+    // 挑完照片之後重新啟動 app，並且將選擇的檔案放在參數中
+    if (args.detail.constructor === Windows.UI.WebUI.WebUIFileOpenPickerContinuationEventArgs) {
+      var files = args.detail.files;
+      if (files.length > 0) {
+        var file = files[0];
+
+        // 將傳回的檔案指標轉成 data URI (data:image/jpg;xxxxxxx)
+        var objectURL = window.URL.createObjectURL(file, { oneTimeOnly: true });
+
+        // 產生 HTML Image 物件來處理，以便 canvas 繪製
+        var img = new Image();
+        img.src = objectURL;
+        img.onload = function () {
+          // 畫圖
+          var ctx = document.getElementById('canvas').getContext('2d');
+          ctx.drawImage(img, 0, 0, 400, 300);
+
+          // 畫字
+          ctx.font = '36px Segou UI';
+          ctx.fillStyle = '#000';
+          ctx.fillText(document.getElementById('Text').value, 10, 200);
+        };
+      }
+    }
+  };
+
+  app.oncheckpoint = function (args) {
+
+  };
+
+  app.start();
+})();
